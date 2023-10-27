80062ae21e342a2f53857d089fc5e1344619c4f3
jhuber1155 <141798215+jhuber1155@users.noreply.github.com>
2023-10-26 21:06:50 -0700

    Added more initial files and started building login page

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const withAuth = require('../utils/auth');
+ const { Blog } = require('../models');
+
+ router.get('/', (req, res) => {
+
+ })
+
+ module.exports = router;
